69f353b2b345de46a648622536fc6d6525918a84
daniel <danieruru@gmail.com>
2011-11-27 04:42:32 -0800

    should work

--- musasim/hardware/board.h
+++ musasim/hardware/board.h
@@ -1,20 +1,3 @@
-/* Read/write macros */
-#define READ_BYTE(BASE, ADDR) (BASE)[ADDR]
-#define READ_WORD(BASE, ADDR) (((BASE)[ADDR]<<8) |			\
-							  (BASE)[(ADDR)+1])
-#define READ_LONG(BASE, ADDR) (((BASE)[ADDR]<<24) |			\
-							  ((BASE)[(ADDR)+1]<<16) |		\
-							  ((BASE)[(ADDR)+2]<<8) |		\
-							  (BASE)[(ADDR)+3])
-
-#define WRITE_BYTE(BASE, ADDR, VAL) (BASE)[ADDR] = (VAL) & 0xff
-#define WRITE_WORD(BASE, ADDR, VAL) (BASE)[ADDR] = ((VAL)>>8) & 0xff;		\
-									(BASE)[(ADDR)+1] = (VAL)&0xff
-#define WRITE_LONG(BASE, ADDR, VAL) (BASE)[ADDR] = ((VAL)>>24) & 0xff;		\
-									(BASE)[(ADDR)+1] = ((VAL)>>16)&0xff;	\
-									(BASE)[(ADDR)+2] = ((VAL)>>8)&0xff;		\
-									(BASE)[(ADDR)+3] = (VAL)&0xff
-
 #define NUM_SLOTS 8
 
 #include "cards/card.h"
@@ -28,8 +11,6 @@ void cpu_write_byte(unsigned int address, unsigned int value);
 void cpu_write_word(unsigned int address, unsigned int value);
 void cpu_write_long(unsigned int address, unsigned int value);
 
-
-
 #define SLOT_ADDRESS_MASK 0x1FFFFF
 
 /* Magic locations */
@@ -37,10 +18,5 @@ void cpu_write_long(unsigned int address, unsigned int value);
 #define OFFSET_MAGIC 0x200000
 #define MAX_MAGIC (OFFSET_MAGIC + (SIZE_MAGIC - 1))
 
-
-
-
-
-unsigned int g_fc;								/* Current function code from CPU */
-
+unsigned int g_fc; /* Current function code from CPU */
 
--- musasim/hardware/cards/romcard.c
+++ musasim/hardware/cards/romcard.c
@@ -5,18 +5,20 @@
  *      Author: daniel
  */
 
-#include "romcard.h"
 #include "stdio.h"
 #include "string.h"
 #include "stdbool.h"
 #include "stdint.h"
 
-unsigned char g_rom[SIZE_ROM]; /* ROM */
-unsigned char g_ram[SIZE_RAM]; /* RAM */
+#include "romcard.h"
+#include "../util.h"
+
+uint8_t rom[SIZE_ROM]; /* ROM */
+uint8_t ram[SIZE_RAM]; /* RAM */
 
 bool romcard_loadrom(char* path) {
 
-	memset(g_rom, 0x00, SIZE_ROM);
+	memset(rom, 0x00, SIZE_ROM);
 	FILE* fhandle;
 	if ((fhandle = fopen(path, "rb")) == NULL) {
 		printf("Unable to open %s\n", path);
@@ -24,7 +26,7 @@ bool romcard_loadrom(char* path) {
 	}
 
 	printf("Loading %s into ROM\n", path);
-	if (fread(g_rom, 1, SIZE_ROM + 1, fhandle) <= 0) {
+	if (fread(rom, 1, SIZE_ROM + 1, fhandle) <= 0) {
 		printf("Error reading %s\n", path);
 		return false;
 	}
@@ -32,27 +34,90 @@ bool romcard_loadrom(char* path) {
 	return true;
 }
 
+bool romcard_valid_address(uint32_t address, bool write) {
+	if (address < MAX_RAM) {
+		return true;
+	}
+
+	return false;
+}
+
+typedef struct {
+	uint8_t* base;
+	uint32_t relative_address;
+} bank;
+
+bank romcard_bank(uint32_t address) {
+
+	bank reg;
+
+	if (address >= OFFSET_ROM && address <= MAX_ROM) {
+		reg.base = rom;
+		reg.relative_address = address - OFFSET_ROM;
+	}
+
+	else if (address >= OFFSET_RAM && address <= MAX_RAM) {
+		reg.base = ram;
+		reg.relative_address = address - OFFSET_RAM;
+	}
+
+	return reg;
+}
+
 uint8_t romcard_read_byte(uint32_t address) {
+
+	if (romcard_valid_address(address, false)) {
+		bank reg = romcard_bank(address);
+		return READ_BYTE((reg.base), (reg.relative_address));
+	}
+
 	return 0;
 }
 
 uint16_t romcard_read_word(uint32_t address) {
+
+	if (romcard_valid_address(address, false)) {
+		bank reg = romcard_bank(address);
+		return READ_WORD(reg.base, reg.relative_address);
+	}
+
 	return 0;
 }
 
 uint32_t romcard_read_long(uint32_t address) {
+
+	if (romcard_valid_address(address, true)) {
+		bank reg = romcard_bank(address);
+		return READ_LONG(reg.base, reg.relative_address);
+	}
+
 	return 0;
 }
 
 void romcard_write_byte(uint32_t address, uint8_t value) {
 
+	if (romcard_valid_address(address, true)) {
+		bank reg = romcard_bank(address);
+		WRITE_BYTE(reg.base, reg.relative_address, value);
+	}
+
 }
 
 void romcard_write_word(uint32_t address, uint16_t value) {
 
+	if (romcard_valid_address(address, true)) {
+		bank reg = romcard_bank(address);
+		WRITE_WORD(reg.base, reg.relative_address, value);
+	}
+
 }
 
 void romcard_write_long(uint32_t address, uint32_t value) {
+
+	if (romcard_valid_address(address, true)) {
+		bank reg = romcard_bank(address);
+		WRITE_LONG(reg.base, reg.relative_address, value);
+	}
 
 }
 
