41dcaca7dcfaafeec41c7ed8d864a4fc3122e6fb
daniel <danieruru@gmail.com>
2011-12-05 04:50:48 -0800

    flesh this out a bit, does nothing ;)

--- musasim/hardware/cards/inputcard.c
+++ musasim/hardware/cards/inputcard.c
@@ -7,8 +7,26 @@
 
 #include "inputcard.h"
 
-uint8_t inputcard_read_byte() {
-	return 0;
+uint8_t port0 = 0xff;
+uint8_t port1 = 0xff;
+
+uint8_t inputcard_read_byte(uint32_t address) {
+
+	int port = address & 0x1;
+
+	switch (port) {
+		case 0x0:
+			return port0;
+		case 0x1:
+			return port1;
+		default:
+			return 0;
+	}
+
 }
 
-card inputcard = { "INPUT CARD", NULL, NULL, NULL, inputcard_read_byte, NULL, NULL, NULL, NULL, NULL };
+void inputcard_tick() {
+	// "latch" stuff here
+}
+
+card inputcard = { "INPUT CARD", NULL, NULL, inputcard_tick, inputcard_read_byte, NULL, NULL, NULL, NULL, NULL };
