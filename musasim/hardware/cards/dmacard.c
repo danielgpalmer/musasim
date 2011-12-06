711f742233b7a644763b5c7b40d2ffc0fea717a5
daniel <danieruru@gmail.com>
2011-12-06 05:24:26 -0800

    lets have a dma controller "card" too

---
+++ musasim/hardware/cards/dmacard.c
@@ -0,0 +1,15 @@
+/*
+ * dmacard.c
+ *
+ *  Created on: Dec 6, 2011
+ *      Author: daniel
+ */
+
+#ifndef DMACARD_C_
+#define DMACARD_C_
+
+#include "dmacard.h"
+
+card dmacard = { "DMA Controller", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL };
+
+#endif /* DMACARD_C_ */
---
+++ musasim/hardware/cards/dmacard.h
@@ -0,0 +1,15 @@
+/*
+ * dmacard.h
+ *
+ *  Created on: Dec 6, 2011
+ *      Author: daniel
+ */
+
+#ifndef DMACARD_H_
+#define DMACARD_H_
+
+#include "card.h"
+
+card dmacard;
+
+#endif /* DMACARD_H_ */
