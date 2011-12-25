082f22179295f875d67efc454c7b0377fbe6d611
daniel <danieruru@gmail.com>
2011-12-25 04:49:13 -0800

    logging stuff

--- musasim/logging.c
+++ musasim/logging.c
@@ -7,8 +7,11 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "logging.h"
+
+#define LONGESTTAG 10
 
 static int loglevel = LEVEL_DEBUG;
 
@@ -21,18 +24,30 @@ void log_println(int level, const char* tag, char * fmt, ...) {
 		return;
 	}
 
+	int taglen = strlen(tag);
+
 	va_list argptr;
 	va_start(argptr, fmt);
 
 	if (level == LEVEL_WARNING) {
-		printf("\033[0;40;%sm", RED);
+		printf("\033[1;40;%sm", RED);
 	}
 
-	if (level == LEVEL_DEBUG) {
-		printf("\033[0;40;%sm", YELLOW);
+	else if (level == LEVEL_DEBUG) {
+		printf("\033[1;40;%sm", YELLOW);
 	}
 
-	printf("[%s]\t", tag);
+	else {
+		printf("\033[1m");
+	}
+
+	printf("[%s]", tag);
+
+	// FIXME this is shit
+	for (int i = 0; i < (LONGESTTAG - taglen); i++) {
+		printf(" ");
+	}
+
 	printf("\033[0m");
 	vprintf(fmt, argptr);
 	printf("\n");
