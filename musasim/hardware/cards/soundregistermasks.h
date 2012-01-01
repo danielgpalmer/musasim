f2fd30729b5e70c63f138261bb43d06dfcef1ece
daniel <danieruru@gmail.com>
2012-01-01 21:08:11 +0900

    missing file

---
+++ musasim/hardware/cards/soundregistermasks.h
@@ -0,0 +1,15 @@
+/*
+ * soundregistermasks.h
+ *
+ *  Created on: Jan 1, 2012
+ *      Author: daniel
+ */
+
+#ifndef SOUNDREGISTERMASKS_H_
+#define SOUNDREGISTERMASKS_H_
+
+#define SOUND_CHANNEL_ENABLED 0b1
+#define SOUND_CHANNEL_LEFT 0b1000
+#define SOUND_CHANNEL_RIGHT 0b100
+
+#endif /* SOUNDREGISTERMASKS_H_ */
