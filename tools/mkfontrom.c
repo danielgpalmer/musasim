c03c96fb4d005af32de66f904ecb4e1e400af7af
daniel <danieruru@gmail.com>
2012-01-21 17:39:36 +0900

    option to generate ASCII or KANA sets

--- tools/mkfontrom.c
+++ tools/mkfontrom.c
@@ -8,6 +8,12 @@
 
 int pixelwidth, pixelheight;
 
+typedef enum {
+	ASCII, KANA
+} charset;
+
+charset charst = ASCII;
+
 void renderbitmap(FT_GlyphSlot slot, FILE* output) {
 
 	uint8_t chardata[(pixelwidth / 8) * pixelheight];
@@ -88,16 +94,47 @@ void renderbitmap(FT_GlyphSlot slot, FILE* output) {
 	fwrite(chardata, 1, sizeof(chardata), output);
 }
 
+void renderchar(uint32_t ch, FT_Face face, FILE* output) {
+
+	int error;
+
+	printf("char is %c\n", (char) ch);
+
+	FT_UInt glyph_index = FT_Get_Char_Index(face, ch);
+
+	error = FT_Load_Glyph(face, /* handle to face object */
+	glyph_index, /* glyph index           */
+	0); /* load flags, see below */
+	if (error) {
+		printf("error loading glyph\n");
+		return;
+	}
+
+	if (face->glyph->format != FT_GLYPH_FORMAT_BITMAP) {
+		printf("glyph is not bitmap\n");
+		error = FT_Render_Glyph(face->glyph, /* glyph slot  */
+		FT_RENDER_MODE_MONO); /* render mode */
+		if (error) {
+			printf("couldnt render\n");
+			return;
+		}
+	}
+
+	renderbitmap(face->glyph, output);
+
+}
+
 int main(int argc, char* argv[]) {
 
 	struct arg_lit *help = arg_lit0(NULL, "help", "print this help and exit");
+	struct arg_str *chars = arg_str0("c", "charset", "charset", "charset to generate, ascii or kana");
 	struct arg_int *w = arg_int1("w", "width", "integer", "the width of the chars");
 	struct arg_int *h = arg_int1("h", "height", "integer", "the height of the chars");
 	struct arg_file *fontfile = arg_file1("f", "font", "file", "the font you want to use");
 	struct arg_file *outputfile = arg_file1("o", "output", "file", "Where the generated font binary should go");
 	struct arg_end *end = arg_end(20);
 
-	void *argtable[] = { help, w, h, fontfile, outputfile, end };
+	void *argtable[] = { help, chars, w, h, fontfile, outputfile, end };
 
 	if (arg_nullcheck(argtable) != 0) {
 		/* NULL entries were detected, some allocations must have failed */
@@ -120,6 +157,18 @@ int main(int argc, char* argv[]) {
 		arg_print_errors(stdout, end, argv[0]);
 		printf("Try '%s --help' for more information.\n", argv[0]);
 		return 1;
+	}
+
+	if (chars->count == 1) {
+		if (strcmp(*chars->sval, "ascii") == 0) {
+		}
+		else if (strcmp(*chars->sval, "kana") == 0) {
+			charst = KANA;
+		}
+		else {
+			printf("bad charset");
+			return 1;
+		}
 	}
 
 	pixelwidth = *w->ival;
@@ -152,31 +201,25 @@ int main(int argc, char* argv[]) {
 	pixelwidth, /* pixel_width           */
 	pixelheight - 4); /* pixel_height          */
 
-	for (int i = 32; i <= 126; i++) { // FIXME not right?
+	switch (charst) {
+		case ASCII:
+			for (int i = 32; i <= 126; i++) { // FIXME not right?
+				renderchar(i, face, output);
+			}
+			break;
+		case KANA: {
+			// http://tlt.its.psu.edu/suggestions/international/bylanguage/japanesechartkatakana.html
+			uint32_t punctuation[] = { 0x309B, 0x309C, 0x30FC, 0x30FD, 0x30FE };
 
-		printf("char is %c\n", (char) i);
+			for (int i = 0; i < sizeof(punctuation) / sizeof(uint32_t); i++) {
+				renderchar(punctuation[i], face, output);
+			}
 
-		FT_UInt glyph_index = FT_Get_Char_Index(face, i);
-
-		error = FT_Load_Glyph(face, /* handle to face object */
-		glyph_index, /* glyph index           */
-		0); /* load flags, see below */
-		if (error) {
-			printf("error loading glyph\n");
-			return 1;
-		}
-
-		if (face->glyph->format != FT_GLYPH_FORMAT_BITMAP) {
-			printf("glyph is not bitmap\n");
-			error = FT_Render_Glyph(face->glyph, /* glyph slot  */
-			FT_RENDER_MODE_MONO); /* render mode */
-			if (error) {
-				printf("couldnt render\n");
-				return 1;
+			for (int i = 0x30a1; i <= 0x30FB; i++) {
+				renderchar(i, face, output);
 			}
 		}
-
-		renderbitmap(face->glyph, output);
+			break;
 	}
 
 	fclose(output);
