19a6a0bf6dd2b55436387e083ebefcaa2eb10122
daniel <danieruru@gmail.com>
2012-03-11 14:16:14 +0900

    seems SDL2 needs 2.2 :/

---
+++ android/jni/SDL/Android.mk
@@ -0,0 +1,7 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libSDL2
+LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libSDL2.so
+
+include $(PREBUILT_SHARED_LIBRARY)
---
+++ android/jni/src/main.c
@@ -0,0 +1,9 @@
+#include <SDL_main.h>
+
+#ifdef __cplusplus
+  extern "C"
+#endif
+   int main(int argc, char *argv[])
+   {
+     
+  }
