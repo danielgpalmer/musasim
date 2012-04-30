03dfc309e3a9621e710670e6c9430e892e8fa2b4
daniel <danieruru@gmail.com>
2012-04-30 19:09:36 +0900

    use logging

--- musasim/elfloader.c
+++ musasim/elfloader.c
@@ -11,6 +11,9 @@
 #include <fcntl.h>
 #include "elfloader.h"
 #include <stdio.h>
+#include "logging.h"
+
+static const char TAG[] = "elfloader";
 
 bool elf_load(const char* path, uint8_t* dest, uint32_t destaddr, int maxlen) {
 	FILE* file;
@@ -19,52 +22,53 @@ bool elf_load(const char* path, uint8_t* dest, uint32_t destaddr, int maxlen) {
 	GElf_Ehdr ehdr;
 
 	if (elf_version(EV_CURRENT) == EV_NONE) {
-		printf("libelf init failed\n");
+		log_println(LEVEL_WARNING, TAG, "libelf init failed");
 		return false;
 	}
 
 	if (!(file = fopen(path, "r"))) {
-		printf("Failed to open file %s\n", path);
+		log_println(LEVEL_WARNING, TAG, "Failed to open file %s", path);
 		return false;
 	}
 
 	if ((e = elf_begin(file->_fileno, ELF_C_READ, NULL)) == NULL) {
-		printf("Failed to open elf\n");
+		log_println(LEVEL_WARNING, TAG, "libelf failed to parse %s", path);
 		return false;
 	}
 
 	if (gelf_getehdr(e, &ehdr) == NULL) {
-		printf("Failed to get elf header!\n");
+		log_println(LEVEL_WARNING, TAG, "Failed to get elf header!");
 		return false;
 	}
 
 	if (ehdr.e_machine != EM_68K) {
-		printf("Elf is not for M68K!\n");
+		log_println(LEVEL_WARNING, TAG, "Elf is not for M68K!");
 		return false;
 	}
-
-	printf("This elf has %d prog headers\n", ehdr.e_phnum);
 
 	if (ehdr.e_phnum == 0) {
-		printf("this elf file has no prog headers!! not an elf file?\n");
+		log_println(LEVEL_WARNING, TAG, "this elf file has no prog headers!! not an elf file?\n");
 		return false;
 	}
+
+	log_println(LEVEL_INFO, TAG, "This elf has %d prog headers", ehdr.e_phnum);
 
 	for (int header = 0; header < ehdr.e_phnum; header++) {
 		GElf_Phdr phdr;
 		gelf_getphdr(e, header, &phdr);
 		if (phdr.p_type == 0x1) {
 
-			printf("Phy address 0x%08lx, Virt address 0x%08lx\n", phdr.p_paddr, phdr.p_vaddr);
+			log_println(LEVEL_INFO, TAG, "Phy address 0x%08lx, Virt address 0x%08lx\n", phdr.p_paddr, phdr.p_vaddr);
 			if (phdr.p_paddr >= destaddr + maxlen) {
-				printf("Section is past the end of the area being loaded to, ignored\n");
+				log_println(LEVEL_INFO, TAG, "Section is past the end of the area being loaded to, ignored\n");
 				break;
 			}
 
 			if (fseek(file, phdr.p_offset, SEEK_SET) != 0) {
 				printf("Failed to seek\n");
+				return false;
 			}
-			printf("Read %ld bytes\n", fread(dest + (phdr.p_paddr - destaddr), 1, phdr.p_filesz, file));
+			fread(dest + (phdr.p_paddr - destaddr), 1, phdr.p_filesz, file);
 
 		}
 
