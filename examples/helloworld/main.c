68d38f61e5398e90aaf48de3d0b38fbc144e8e69
daniel <danieruru@gmail.com>
2012-04-30 19:24:20 +0900

    add an example for an user

---
+++ examples/helloworld/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 17, 2011
+ *      Author: daniel
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+
+	while(1){
+		printf("Hello, world!\n");
+	}
+	return 0;
+}
