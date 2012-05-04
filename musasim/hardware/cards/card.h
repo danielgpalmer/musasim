51b1af6660bff47399a134e572f2ad30c76b1edf
daniel <danieruru@gmail.com>
2012-05-04 20:24:42 +0900

    some less duplicates

--- musasim/hardware/cards/card.h
+++ musasim/hardware/cards/card.h
@@ -13,17 +13,17 @@
 #include <stdbool.h>
 
 typedef struct {
-	char* boardinfo;
-	void (*init)();
-	void (*dispose)();
-	void (*reset)();
-	void (*tick)();
+	char* boardinfo; // A simple description that can be displayed to aid debugging
+	void (*init)(); // Do anything you need to do at program startup here
+	void (*dispose)(); // Do anything you need to do before the program exits, i.e. free'ing stuff, here
+	void (*reset)(); // This is called when the m68k pulls the reset line down
+	void (*tick)(); //
 	void (*irqack)();
 	void (*busreqack)();
-	bool (*validaddress)(uint32_t address);
-	uint8_t (*read_byte)(uint32_t address);
-	uint16_t (*read_word)(uint32_t address);
-	uint32_t (*read_long)(uint32_t address);
+	bool (*validaddress)(uint32_t address); // This should return true if the address is valid within the card's address space
+	uint8_t (*read_byte)(uint32_t address); // Read a byte from the cards address space
+	uint16_t (*read_word)(uint32_t address); // ^ but for word (16bits)
+	uint32_t (*read_long)(uint32_t address); // ^ but for long word (32bits)
 	void (*write_byte)(uint32_t address, uint8_t value);
 	void (*write_word)(uint32_t address, uint16_t value);
 	void (*write_long)(uint32_t address, uint32_t value);
