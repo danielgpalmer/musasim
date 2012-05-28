9a19dff65aba0edcba46e533eac97aedc9b01a4a
daniel <danieruru@gmail.com>
2012-05-28 22:42:17 +0900

    clean out unneeded shit

--- musasim/hardware/cards/basicsound.c
+++ musasim/hardware/cards/basicsound.c
@@ -5,18 +5,14 @@
  *      Author: daniel
  */
 
+#include <stdint.h>
 #include "basicsound.h"
 #include "../../logging.h"
 #include "../board.h"
 
 static const char TAG[] = "basicsound";
 
-static void basicsound_init() {
-
-}
-
-static void basicsound_dispose() {
-}
+static uint16_t divisor = 0;
 
 static bool basicsound_validaddress(uint32_t address) {
 	return true;
@@ -27,17 +23,12 @@ static void basicsound_tick() {
 }
 
 static void basicsound_write_word(uint32_t address, uint16_t data) {
-
+	divisor = data;
 }
 
 static uint16_t basicsound_read_word(uint32_t address) {
-	return 0;
+	return divisor;
 }
 
-static void basicsound_irqack() {
-	board_lower_interrupt(&basicsoundcard);
-}
-
-const card basicsoundcard = { "BASIC SOUND CARD", basicsound_init, basicsound_dispose, NULL, basicsound_tick,
-		basicsound_irqack, NULL, basicsound_validaddress, NULL, basicsound_read_word, NULL, NULL, basicsound_write_word,
-		NULL };
+const card basicsoundcard = { "BASIC SOUND CARD", NULL, NULL, NULL, basicsound_tick, NULL, NULL,
+		basicsound_validaddress, NULL, basicsound_read_word, NULL, NULL, basicsound_write_word, NULL };
