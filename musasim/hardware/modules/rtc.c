6526273dab63c9d35019bbd773fc556829e6be8f
daniel <danieruru@gmail.com>
2012-09-23 15:57:52 +0900

    base for the rtc

---
+++ musasim/hardware/modules/rtc.c
@@ -0,0 +1,24 @@
+/*
+ * rtc.c
+ *
+ *  Created on: 23 Sep 2012
+ *      Author: daniel
+ */
+
+#define TAG "rtcmodule"
+
+#include <stddef.h>
+#include "rtc.h"
+
+const module rtcmodule = { //
+		TAG, //
+				NULL, //
+				NULL, //
+				NULL, //
+				NULL, //
+				NULL, //
+				NULL, //
+				NULL, //
+				NULL, //
+				NULL //
+		};
---
+++ musasim/hardware/modules/rtc.h
@@ -0,0 +1,15 @@
+/*
+ * rtc.h
+ *
+ *  Created on: 23 Sep 2012
+ *      Author: daniel
+ */
+
+#ifndef RTC_H_
+#define RTC_H_
+
+#include "module.h"
+
+const module rtc;
+
+#endif /* RTC_H_ */
