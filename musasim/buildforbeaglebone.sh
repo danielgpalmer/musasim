2646394c6545d8cd31148d6ebe09fb8b83bf4bb7
Daniel Palmer <daniel@roi.(none)>
2013-02-10 10:38:04 +0900

    Fix opt flags for BB

--- musasim/buildforbeaglebone.sh
+++ musasim/buildforbeaglebone.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-OPT="-O3 -march=armv7-a -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp" make
+OPT="-O3 -march=armv7-a -mtune=cortex-a8 -mfpu=neon -mfloat-abi=hard -funsafe-math-optimizations" make
